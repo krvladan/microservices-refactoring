d209168ec1f6edc38c2121db9ee104389d586a89
Vladan Krstic <6428373+krvladan@users.noreply.github.com>
2020-06-15 20:51:35 +0200

    Environment variables

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=postgres
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=default
+export JWT_SECRET=
+export URL=http://localhost:8100
